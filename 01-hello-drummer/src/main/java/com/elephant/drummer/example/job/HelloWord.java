31abbc2df91ab1b0a0602f36003026377d63bc30
Klaudiusz <Klaudiusz@DESKTOP-1MIMJOG.poland.asseco.corp>
2018-03-23 13:23:55 +0100

    start

---
+++ 01-hello-drummer/src/main/java/com/elephant/drummer/example/job/HelloWord.java
@@ -0,0 +1,27 @@
+package com.elephant.drummer.example.job;
+
+import java.util.logging.Logger;
+
+import com.elephant.drummer.annotation.DrummerJob;
+import com.elephant.drummer.annotation.trigger.Period;
+import com.elephant.drummer.scope.DrummerObservable;
+
+/**
+ * 
+ * @author Klaudiusz Wojtkowiak Elephant Software 2018
+ *
+ */
+public class HelloWord implements DrummerObservable{
+	
+	Logger log=Logger.getLogger(HelloWord.class.getSimpleName());
+	
+	/**
+	 * Basic example how to use Drummer for simple period execution
+	 */
+	@DrummerJob(every=@Period(second=10))
+	public void helloWorld() {
+		log.info("Hello! Your first Drummer Job is working!");
+	}
+	
+
+}
