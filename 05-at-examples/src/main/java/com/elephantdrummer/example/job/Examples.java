92f66202e002317f6d4de74cb05a0e08ee57035a
Klaudiusz <Klaudiusz@DESKTOP-1MIMJOG.poland.asseco.corp>
2018-04-14 20:34:51 +0200

    fix

--- 05-at-examples/src/main/java/com/elephantdrummer/example/job/Examples.java
+++ 05-at-examples/src/main/java/com/elephantdrummer/example/job/Examples.java
@@ -5,6 +5,7 @@ import java.util.logging.Logger;
 import com.elephantdrummer.annotation.DayOfWeek;
 import com.elephantdrummer.annotation.DrummerJob;
 import com.elephantdrummer.annotation.TestJob;
+import com.elephantdrummer.annotation.trigger.After;
 import com.elephantdrummer.annotation.trigger.At;
 import com.elephantdrummer.annotation.trigger.TimeZoneEnum;
 import com.elephantdrummer.scope.DrummerObservable;
@@ -15,7 +16,7 @@ public class Examples implements DrummerObservable{
 	Logger log=Logger.getLogger(Examples.class.getSimpleName());
 	
 	@TestJob(executeJob=false)
-	@DrummerJob(at= @At(hour=2,dayOfWeek=DayOfWeek.FRIDAY))
+	@DrummerJob(at={@At(second=2),@At(second=30)})
 	public void test01() {
 		
 	}
@@ -27,7 +28,7 @@ public class Examples implements DrummerObservable{
 	}
 	
 	@TestJob(executeJob=false)
-	@DrummerJob(at= @At(hour=2,dayOfWeek=DayOfWeek.FRIDAY))
+	@DrummerJob(at=@At(hour=10,minute=14,second=30,dayOfWeek=DayOfWeek.FRIDAY))
 	public void test03() {
 		
 	}
